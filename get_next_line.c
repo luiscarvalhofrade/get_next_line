cd429937ba8c38c7b222d4c2f0630423fbde9b5e
Luis Claudio De Carvalho Frade <luide-ca@c3r5p1.42sp.org.br>
2024-11-11 14:12:02 -0300

    first try for gnl

--- get_next_line.c
+++ get_next_line.c
@@ -10,4 +10,99 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+void	polish_list(t_list **list)
+{
+	t_list	*last_node;
+	t_list	*clean_node;
+	int		i;
+	int		k;
+	char	*buf;
+
+	buf = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	clean_node = (t_list *)malloc(sizeof(t_list));
+	if (!buf || !clean_node)
+		return ;
+	last_node = find_last_node(*list);
+	i = 0;
+	k = 0;
+	while (last_node->str_buf[i] != '\0'
+		&& last_node->str_buf[i] != '\n')
+		i++;
+	while (last_node->str_buf[i] != '\0'
+		&& last_node->str_buf[++i])
+		buf[k++] = last_node->str_buf[i];
+	buf[k] = '\0';
+	clean_node->str_buf = buf;
+	clean_node->next = NULL;
+	dealloc(list, clean_node, buf);
+}
+
+char	*get_line(t_list *list)
+{
+	int		str_len;
+	char	*next_str;
+
+	if (!list)
+		return (0);
+	str_len = len_to_newline(list);
+	next_str = (char *)malloc((str_len + 1) * sizeof(char));
+	if (!next_str)
+		return (0);
+	copy_str(list, next_str);
+	return (next_str);
+}
+
+void	append(t_list **list, char *buf)
+{
+	t_list	*new_node;
+	t_list	*last_node;
+
+	last_node = find_last_node(*list);
+	new_node = (t_list *)malloc(sizeof(t_list));
+	if (!new_node)
+		return ;
+	if (!last_node)
+		*list = new_node;
+	else
+		last_node->next = new_node;
+	new_node->str_buf = buf;
+	new_node->next = NULL;
+}
+
+void	create_list(t_list **list, int fd)
+{
+	int		char_read;
+	char	*buf;
+
+	while (!found_new_line(*list))
+	{
+		buf = (char *)malloc(BUFFER_SIZE + 1);
+		if (!buf)
+			return ;
+		char_read = read(fd, buf, BUFFER_SIZE);
+		if (!char_read)
+		{
+			free(buf);
+			return ;
+		}
+		buf[char_read] = '\0';
+		append(list, buf);
+	}
+}
+
+char	*get_next_line(int fd)
+{
+	static t_list	*list = NULL;
+	char			*next_line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
+		return (0);
+	create_list(&list, fd);
+	if (!list)
+		return (0);
+	next_line = get_line(list);
+	polish_list(&list);
+	return (next_line);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -10,4 +10,29 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_list
+{
+	char			*str_buf;
+	struct s_list	*next;
+}					t_list;
+
+int		found_newline(t_list *list);
+int		len_to_new_line(t_list *list);
+
+void	copy_str(t_list *list, char *str);
+void	dealloc(t_list **list, t_list *clean_node, char *buf);
+
+t_list	*find_last_node(t_list *list);
+
+#endif
